6bf0a516b525e172c89572b422bdd7127e74af7d
PeterEB <peter.eb9@gmail.com>
2017-07-07 19:15:25 +0800

    add ct devAttrs, bump to v0.2.5

--- example/clientTest.js
+++ example/clientTest.js
@@ -65,17 +65,17 @@ coapNode.on('error', function (err) {
     console.log(err);
 });
 
-coapNode.register('leshan.eclipse.org', 5683, function (err, rsp) {
-    console.log(rsp);
-});
-
-// coapNode.register('127.0.0.1', 5683, function (err, rsp) {
+// coapNode.register('leshan.eclipse.org', 5683, function (err, rsp) {
 //     console.log(rsp);
 // });
 
-setInterval(function () {
-    so.read(3303, 0, 5702, function () {});
-}, 3000);
+coapNode.register('127.0.0.1', 5683, function (err, rsp) {
+    console.log(rsp);
+});
+
+// setInterval(function () {
+//     so.read(3303, 0, 5702, function () {});
+// }, 3000);
 
 // setTimeout(function () {
 //     coapNode.register('127.0.0.1', 5683, function (err, rsp) {
